1e187d7485b2e8027bdc8441d404ba02542a3f9e
asif-b190314 <84712554+asif-b190314@users.noreply.github.com>
2021-05-31 16:20:54 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -15,8 +15,8 @@ int main(void) {
   float pi0;
   float pi1;
   
-  pi0 = mc_pi(25000);
-  pi1 = mc_pi(25000);
+  pi0 = mc_pi(250000);
+  pi1 = mc_pi(250000);
   printf("%f %f\n", pi0, pi1);
   
   if (pi0 == pi1) {
